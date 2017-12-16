1a21043b4dd411705b1a06c6d4609fd6f2aeb6a4
akhiln66 <30778439+akhiln66@users.noreply.github.com>
2017-12-16 17:48:07 +0530

    added preloader

--- js/main.js
+++ js/main.js
@@ -12,7 +12,28 @@ $(document).ready(function () {
 function main() {
 
 (function () {
-   'use strict';
+    'use strict';
+
+    /*====================================
+					Preloader
+		======================================*/
+
+    $(window).load(function () {
+
+        var preloaderDelay = 350,
+            preloaderFadeOutTime = 800;
+
+        function hidePreloader() {
+            var loadingAnimation = $('#loading-animation'),
+                preloader = $('#preloader');
+
+            loadingAnimation.fadeOut();
+            preloader.delay(preloaderDelay).fadeOut(preloaderFadeOutTime);
+        }
+
+        hidePreloader();
+
+    });
 
    /* ==============================================
   	Testimonial Slider
