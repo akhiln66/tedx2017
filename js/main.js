81d11309befd2526f1cb76f9c6114267e96efcf1
akhiln66 <30778439+akhiln66@users.noreply.github.com>
2017-12-16 16:29:13 +0530

    pre loader

--- js/main.js
+++ js/main.js
@@ -88,7 +88,28 @@ function main() {
         singleItem:true
         });
 
-  	});
+        });
+
+        /*====================================
+                        Preloader
+            ======================================*/
+
+        $(window).load(function () {
+
+            var preloaderDelay = 350,
+                preloaderFadeOutTime = 800;
+
+            function hidePreloader() {
+                var loadingAnimation = $('#loading-animation'),
+                    preloader = $('#preloader');
+
+                loadingAnimation.fadeOut();
+                preloader.delay(preloaderDelay).fadeOut(preloaderFadeOutTime);
+            }
+
+            hidePreloader();
+
+        });
 
   	/*====================================
     Portfolio Isotope Filter
